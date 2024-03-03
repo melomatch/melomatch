7db9b84947f18ea79ac290f70295446ef10c63a3
dasha <dasha.ved93@gmail.com>
2024-03-03 21:23:36 +0300

    chore: corrections after the pull request
    Refs: #15

--- melomatch/settings.py
+++ melomatch/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'melomatch.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": env("DB_NAME", "melomatch"),
         "USER": env("DB_USER", "melomatch"),
         "PASSWORD": env("DB_PASSWORD", "melomatch"),
