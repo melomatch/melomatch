55b18184f999e21a5c7625c22b97c99e69eb2d74
diyanovamir <amirdianov@gmail.com>
2024-03-20 23:35:12 +0300

    feat: django models without custom user model
    Refs: #18

--- web/models.py
+++ web/models.py
@@ -1,1 +1,42 @@
 # Create your models here.
+from django.db import models
+
+
+class Track(models.Model):
+    yandex_id = models.IntegerField(verbose_name="ID в Yandex")
+    title = models.CharField(verbose_name="Название")
+    release_date = models.DateTimeField(max_length=255, verbose_name="Дата выхода")
+    cover = models.URLField(max_length=511, verbose_name="URL обложки")
+    genre = models.ManyToManyField("Genre", on_delete=models.CASCADE, verbose_name="Жанр")
+    artist = models.ManyToManyField("Artist", on_delete=models.CASCADE, verbose_name="Артист")
+
+    class Meta:
+        verbose_name = "песня"
+        verbose_name_plural = "песни"
+
+    def __str__(self):
+        return self.title
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=63, verbose_name="Жанр")
+
+    class Meta:
+        verbose_name = "жанр"
+        verbose_name_plural = "жанры"
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(models.Model):
+    yandex_id = models.IntegerField(verbose_name="ID в Yandex")
+    name = models.CharField(max_length=255, verbose_name="Имя")
+    avatar = models.CharField(max_length=511, verbose_name="URL фотографии профиля")
+
+    class Meta:
+        verbose_name = "артист"
+        verbose_name_plural = "артисты"
+
+    def __str__(self):
+        return self.name
