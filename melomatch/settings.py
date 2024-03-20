622a5416aa1a0f90db704f1d43819e52b705d921
diyanovamir <amirdianov@gmail.com>
2024-03-20 23:07:24 +0300

    build: add package python-dotenv
    Refs: #18

--- melomatch/settings.py
+++ melomatch/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from os import getenv as env
 from pathlib import Path
 
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -27,7 +30,6 @@ SECRET_KEY = env("SECRET_KEY", "django-insecure-9a!%cvzy)hsjsnv$#mhgeq%ja&erz=6f
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "melomatch.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -85,7 +86,6 @@ DATABASES = {
         "PORT": env("DB_PORT", "5432"),
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -105,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -116,7 +115,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
