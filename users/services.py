3dbda23a712d6c4c77aa6e0215c2e80c54860221
Vitaly Zorin <addefan@mail.ru>
2024-03-23 00:06:40 +0300

    feat: add function to create user by yandex user data
    Refs: #23

--- users/services.py
+++ users/services.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 from http import HTTPStatus
 
 import requests
+
+from users.enums import Sex
+from users.models import User
 
 
 def get_user_info_by_yandex_token(token):
@@ -14,3 +18,28 @@ def get_user_info_by_yandex_token(token):
         return "Не удалось получить информацию о пользователе", False
 
     return response.json(), True
+
+
+# TODO: функция prepair_yandex_user_data для переименования атрибутов и
+#  обработки необычных случаев (тудушки снизу)
+
+
+def get_user_by_yandex_data(data):
+    try:
+        user = User.objects.get(yandex_id=int(data["id"]))
+    except User.DoesNotExist:
+        user = User(
+            yandex_id=int(data["id"]),
+            username=data["login"],
+            first_name=data["first_name"],
+            last_name=data["last_name"],
+            email=data["default_email"],
+            # TODO: Обработать следующий случай: "Неизвестные части даты
+            #  заполняются нулями, например: 0000-12-23"
+            birthday=datetime.strptime(data["birthday"], "%Y-%m-%d"),
+            # TODO: Обработать None
+            sex=Sex(data["sex"][0].capitalize()),
+            avatar=f"https://avatars.yandex.net/get-yapic/{data["default_avatar_id"]}",
+        )
+    user.save()
+    return user
