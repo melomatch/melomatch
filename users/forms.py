3e4ac8ef9069a6546acdef270a7f0cc7a7976569
dasha <dasha.ved93@gmail.com>
2024-03-28 14:59:13 +0300

    refactor: some changes after review
    Refs: #25

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,9 @@
-from django.forms import DateInput, ModelForm
+from django.forms import DateInput, EmailInput, ModelForm
 
 from users.models import User
 
 
-class DefaultBulmaInputs:
+class BulmaFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.label_suffix = ""
@@ -11,11 +11,11 @@ class DefaultBulmaInputs:
             self.fields[attr].widget.attrs.update({"class": "input"})
 
 
-class ProfileForm(DefaultBulmaInputs, ModelForm):
+class ProfileForm(BulmaFormMixin, ModelForm):
     class Meta:
         model = User
         fields = ("username", "email", "first_name", "last_name", "birthday", "sex")
         widgets = {
             "birthday": DateInput(attrs={"type": "date"}),
-            "email": DateInput(attrs={"type": "email"}),
+            "email": EmailInput(),
         }
