5850c7a35e1bbddabd55a2bddc3a244ddd60d95e
diyanovamir <amirdianov@gmail.com>
2024-04-07 09:42:11 +0300

    fix: path for json file
    Refs: #29

--- web/management/commands/addmusicgenres.py
+++ web/management/commands/addmusicgenres.py
@@ -1,36 +1,16 @@
 import json
 
 from django.core.management import BaseCommand
-from django.utils.translation import gettext_lazy as _
 
 from web.models import Genre
-
-json_obj = {
-    "genres": [
-        {
-            "id": "rusrap",
-            "title": "Русский рэп",
-            "color": "#59cd9c",
-            "cover": "avatars.yandex.net/get-music-misc/34161/rotor-genre-rusrap-icon/%%",
-            "liked": True,
-        },
-        {
-            "id": "rap",
-            "title": "Рэп и хип-хоп",
-            "color": "#59cd9c",
-            "cover": "avatars.yandex.net/get-music-misc/34161/rotor-genre-rusrap-icon/%%",
-            "liked": True,
-        },
-    ]
-}
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Genre.objects.all().delete()
-        with open("genres-yandex.json", encoding="utf-8") as f:
-            info = f.read()
-            obj = json.loads(info)
+        with open("web/management/commands/genres-yandex.json", encoding="utf-8") as f:
+            obj = json.load(f)
         genres = [Genre(title=genre_info["id"]) for genre_info in obj["genres"]]
-        Genre.objects.bulk_create(genres)
-        print(_("Жанры добавлены в таблицу Genre"))
+        Genre.objects.bulk_create(
+            genres, update_conflicts=True, update_fields=["title"], unique_fields=["title"]
+        )
+        print("Жанры добавлены в таблицу Genre")
