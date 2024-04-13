1c485e68d8625ac16dcead36580e8b71272d2051
diyanovamir <amirdianov@gmail.com>
2024-04-13 17:06:19 +0300

    feat: top genres
    Refs: #32

--- web/static/web/js/top_switch.js
+++ web/static/web/js/top_switch.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    let path_name = window.location.pathname;
-    $("#top_list_button").on("click", function () {
-        let show_block = document.querySelector('#top_list_content');
-        let hide_block = document.querySelector('#artist_list_content');
-        show_block.classList.remove('is-hidden')
-        hide_block.classList.add('is-hidden')
-        $(this).addClass('is-selected-switch-button')
-        $("#artist_list_button").removeClass('is-selected-switch-button')
-    });
-    $("#artist_list_button").on("click", function () {
-        let show_block = document.querySelector('#artist_list_content');
-        let hide_block = document.querySelector('#top_list_content');
-        show_block.classList.remove('is-hidden')
-        hide_block.classList.add('is-hidden')
-        $(this).addClass('is-selected-switch-button')
-        $("#top_list_button").removeClass('is-selected-switch-button')
+$(document).ready(function () {
+    function switchContent(showSelector, hideSelector, activeButton, inactiveButton) {
+        let show_block = $(showSelector);
+        let hide_block = $(hideSelector);
+        show_block.removeClass('is-hidden');
+        hide_block.addClass('is-hidden');
+        $(activeButton).addClass('is-selected-switch-button');
+        $(inactiveButton).removeClass('is-selected-switch-button');
+    }
 
+    $("#top_list_button").on("click", function () {
+        switchContent('#top_list_content', '#artist_list_content',
+            '#top_list_button', '#artist_list_button');
     });
-})
+
+    $("#artist_list_button").on("click", function () {
+        switchContent('#artist_list_content', '#top_list_content',
+            '#artist_list_button', '#top_list_button');
+    });
+});
