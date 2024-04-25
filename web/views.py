1c9b7680de2c80d2a17c633f596a741e0ef1f210
Vitaly Zorin <addefan@mail.ru>
2024-04-25 14:03:06 +0300

    fix: genres in top after migrate to fk from m2m
    Refs: #37

--- web/views.py
+++ web/views.py
@@ -31,7 +31,7 @@ class TopListView(TemplateView):
         info = (
             Track.objects.filter(user=self.request.user)
             .prefetch_related("artists")
-            .prefetch_related("genres")
+            .select_related("genre")
         )
         context["top_artists"] = list(
             info.values("artists__name", "artists__avatar")
@@ -39,7 +39,7 @@ class TopListView(TemplateView):
             .order_by("-total")
         )
         context["top_genres"] = list(
-            info.values("genres__title").annotate(total=Count("genres")).order_by("-total")
+            info.values("genre__title").annotate(total=Count("genre__title")).order_by("-total")
         )
         return context
 
