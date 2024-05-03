4a9f9b4bf88399c9d7f04119816f01deefad8ae6
Vitaly Zorin <addefan@mail.ru>
2024-05-03 04:31:08 +0300

    fix: migrations after merge and services __init__ after ruff
    Refs: #41

---
+++ web/services/__init__.py
@@ -0,0 +1,15 @@
+from .compare_tastes import CompareTastesModel
+from .model_instances_machinations import (
+    get_saved_instances_by_unsaved_and_unique_saved,
+    get_unique_model_instances,
+)
+from .yandex_preparers import prepare_artist, prepare_track, prepare_tracks_genres_artists_lists
+
+__all__ = [
+    "get_unique_model_instances",
+    "get_saved_instances_by_unsaved_and_unique_saved",
+    "prepare_track",
+    "prepare_artist",
+    "prepare_tracks_genres_artists_lists",
+    "CompareTastesModel",
+]
